27f3e7a23ff0a934af68f0818258d7d9786454d3
premod <premodsuraweera1@gmail.com>
2023-08-14 15:43:00 +0530

    create model and migration file

---
+++ app/Models/Product.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $filable = [
+        'user_id',
+        'code',
+        'name',
+        'description',
+        'category_id',
+        'display_order_no',
+        'price_created_by',
+    ];
+}
---
+++ database/migrations/2023_08_14_090539_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('code');
+            $table->string('name');
+            $table->text('description');
+            $table->integer('category_id');
+            $table->integer('display_order_no');
+            $table->decimal('price_created_by', 8, 2);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};

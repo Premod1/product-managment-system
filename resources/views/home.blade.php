006d4385d79b6c44b15ccda212c80c06a46ac758
premod <premodsuraweera1@gmail.com>
2023-08-16 15:01:24 +0530

    user display done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,29 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+        <div class="col-md-12">
+            <div class="card text-center">
+                <h4 class="card-header">{{ __('Products Show') }}</h4>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    @foreach ($product as $item )
+                    <div class="card-deck">
+                        <div class="card ">
+                            <img class="card-img-top" src="{{ ('uploads/products/'.$item->image) }}" alt="Product Image"style="max-width: 100%; max-height: 600px;">
+                            <div class="card-body">
+                                <hr/>
+                              <h5>{{ $item->code }}</h5>
+                              <h5 class="card-title"> Product Name : {{ $item->name }}</h5>
+                              <h5>Category Name :{{ $item->category->name }}</h5>
+                              <h5>Price : RS. {{ $item->price_created_by }}</h5>
+                              <hr/>
+                              <p class="card-text">{{ $item->description }}</p>
+                            </div>
+                            <div class="card-footer">
+                                <small class="text-muted">Last updated {{ $item->updated_at->diffForHumans() }}</small>
+                            </div>
+                    </div>
+                    @endforeach
                 </div>
             </div>
         </div>
